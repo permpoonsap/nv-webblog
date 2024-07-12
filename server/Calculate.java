dc7bece959a9c64eddd8bc8ece41d8e73cfcee49
WHmild04 <gmpwh04@gmail.com>
2024-07-12 10:03:11 +0700

    save

---
+++ server/Calculate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Calculate
+{
+   public static void main(String[] args) 
+   {
+      Scanner sc = new Scanner(System.in);
+      int num1, num2;
+      num2 = 6;
+      System.out.print("Enter  value  ");
+      num1 = sc.nextInt();
+      num1 = num1 + 2;
+      num2 = num1 / num2;
+      System.out.println("result  =  " + num2);
+   }
+}
--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,32 @@ let express = require('express');
 const app = express()
 
 app.get('/status', function (req, res){
-    res.send('Hello nodejs server');
+console.log('=====================');
+console.log(req);
+console.log('=====================');
+res.send('Hello nodejs server');
 })
+app.get('/showmyname/:person', function (req, res){
+    console.log('hello - ' + req.params.person)
+    res.send('hello' + "   " + req.params.person);
+    })
+    app.get('/pow2/:person', function (req, res){
+        console.log('hello - ' + req.params.person)
+        res.send(req.params.person + "   " + 'power' + "   " + 2 + ' = ' + (req.params.person*req.params.person));
+        })
+    // app.get('/pow2/:person', function (req, res){
+        
+    //     let person = parseInt(req.params.person);
+    
+    //     let result = Math.pow(person, 2);
+    
+    //     res.send(person + " power 2 = " + result);
+    // });
+    
 
-let port = 8080
+
+let port = 8081
 
 app.listen(port, function () {
-    console.log('server running on' + port)
+    console.log('server running on http://localhost:' + port)
 });
